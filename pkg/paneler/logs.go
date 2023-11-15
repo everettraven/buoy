8ca0306b9597864685d80751cfea1beccd236a83
Bryce Palmer <everettraven@gmail.com>
2023-11-15 11:25:00 -0500

    (feature+bugfix): fetch logs from resources with label selector in spec
    and make log word wrap use the viewport width
    
    Signed-off-by: Bryce Palmer <everettraven@gmail.com>

--- pkg/paneler/logs.go
+++ pkg/paneler/logs.go
@@ -5,20 +5,38 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"strings"
 
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/everettraven/buoy/pkg/charm/models/panels"
 	"github.com/everettraven/buoy/pkg/types"
 	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
 )
 
 var _ Paneler = &Log{}
 
 type Log struct {
-	KubeClient *kubernetes.Clientset
+	typedClient     *kubernetes.Clientset
+	dynamicClient   dynamic.Interface
+	discoveryClient *discovery.DiscoveryClient
+	restMapper      meta.RESTMapper
+}
+
+func NewLog(typedClient *kubernetes.Clientset, dynamicClient dynamic.Interface, discoveryClient *discovery.DiscoveryClient, restMapper meta.RESTMapper) *Log {
+	return &Log{
+		typedClient:     typedClient,
+		dynamicClient:   dynamicClient,
+		discoveryClient: discoveryClient,
+		restMapper:      restMapper,
+	}
 }
 
 func (t *Log) Model(panel types.Panel) (tea.Model, error) {
@@ -27,56 +45,90 @@ func (t *Log) Model(panel types.Panel) (tea.Model, error) {
 	if err != nil {
 		return nil, fmt.Errorf("unmarshalling panel to table type: %s", err)
 	}
-	logItem := modelWrapperForLogPanel(t.KubeClient, log)
-	go streamLogs(t.KubeClient, log, logItem) //nolint: errcheck
+	logItem := modelWrapperForLogPanel(log)
+	pod, err := t.getPodForObject(log)
+	if err != nil {
+		return nil, fmt.Errorf("error getting pod for object: %w", err)
+	}
+	go streamLogs(t.typedClient, pod, logItem, log.Container) //nolint: errcheck
 	return logItem, nil
 }
 
-func modelWrapperForLogPanel(kc *kubernetes.Clientset, logsPanel types.Logs) *panels.Logs {
+func modelWrapperForLogPanel(logsPanel types.Logs) *panels.Logs {
 	vp := viewport.New(100, 20)
 	vpw := panels.NewLogs(logsPanel.Name, vp)
 	return vpw
 }
 
-func streamLogs(kc *kubernetes.Clientset, logsPanel types.Logs, logItem *panels.Logs) {
-	//TODO: expand this beyond just a pod
-	req := kc.CoreV1().Pods(logsPanel.Key.Namespace).GetLogs(logsPanel.Key.Name, &v1.PodLogOptions{
-		Container: logsPanel.Container,
+func (t *Log) getPodForObject(logsPanel types.Logs) (*v1.Pod, error) {
+	gvk := schema.GroupVersionKind{
+		Group:   logsPanel.Group,
+		Version: logsPanel.Version,
+		Kind:    logsPanel.Kind,
+	}
+
+	if gvk == v1.SchemeGroupVersion.WithKind("Pod") {
+		return t.typedClient.CoreV1().Pods(logsPanel.Key.Namespace).Get(context.Background(), logsPanel.Key.Name, metav1.GetOptions{})
+	}
+
+	mapping, err := t.restMapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return nil, fmt.Errorf("error creating resource mapping: %w", err)
+	}
+	u, err := t.dynamicClient.Resource(mapping.Resource).Namespace(logsPanel.Key.Namespace).Get(context.Background(), logsPanel.Key.Name, metav1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("error getting object: %w", err)
+	}
+
+	selector, err := getPodSelectorForUnstructured(u)
+	if err != nil {
+		return nil, fmt.Errorf("error getting pod selector for object: %w", err)
+	}
+	pods, err := t.typedClient.CoreV1().Pods(u.GetNamespace()).List(context.Background(), metav1.ListOptions{LabelSelector: selector.String()})
+	if err != nil {
+		return nil, fmt.Errorf("error getting pods for object: %w", err)
+	}
+	if len(pods.Items) == 0 {
+		return nil, fmt.Errorf("no pods found for object")
+	}
+	return &pods.Items[0], nil
+}
+
+func getPodSelectorForUnstructured(u *unstructured.Unstructured) (labels.Selector, error) {
+	selector, found, err := unstructured.NestedFieldCopy(u.Object, "spec", "selector")
+	if !found {
+		return nil, fmt.Errorf("no pod label selector found in object spec: %s", u.Object)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("error getting pod label selector from object spec: %w", err)
+	}
+	sel := &metav1.LabelSelector{}
+	bytes, err := json.Marshal(selector)
+	if err != nil {
+		return nil, fmt.Errorf("error marshalling selector: %w", err)
+	}
+	err = json.Unmarshal(bytes, sel)
+	if err != nil {
+		return nil, fmt.Errorf("error unmarshalling selector: %w", err)
+	}
+	return metav1.LabelSelectorAsSelector(sel)
+}
+
+func streamLogs(kc *kubernetes.Clientset, pod *v1.Pod, logItem *panels.Logs, container string) {
+	req := kc.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &v1.PodLogOptions{
+		Container: container,
 		Follow:    true,
 	})
 
 	rc, err := req.Stream(context.Background())
 	if err != nil {
-		logItem.AddContent(fmt.Errorf("fetching logs for %s/%s: %w", logsPanel.Key.Namespace, logsPanel.Key.Name, err).Error())
+		logItem.AddContent(fmt.Errorf("fetching logs for %s/%s: %w", pod.Namespace, pod.Name, err).Error())
 		return
 	}
 	defer rc.Close()
 
 	scanner := bufio.NewScanner(rc)
 	for scanner.Scan() {
-		logs := wrapLogs(scanner.Bytes())
-		logItem.AddContent(logs)
+		logItem.AddContent(scanner.Text())
 	}
-}
-
-func wrapLogs(logs []byte) string {
-	logStr := string(logs)
-	splitLogs := strings.Split(logStr, "\n")
-	var logsBuilder strings.Builder
-	for _, log := range splitLogs {
-		if len(log) > 100 {
-			segs := (len(log) / 100)
-			for seg := 0; seg < segs; seg++ {
-				logsBuilder.WriteString(log[:100])
-				logsBuilder.WriteString("\n")
-				log = log[100:]
-			}
-			//write any leftovers
-			logsBuilder.WriteString(log)
-		} else {
-			logsBuilder.WriteString(log)
-		}
-		logsBuilder.WriteString("\n")
-	}
-	return logsBuilder.String()
 }
