594dac8eeb73a779ce057342479a1b99ccde17d2
Bryce Palmer <everettraven@gmail.com>
2023-12-06 11:01:31 -0500

    (build): update version command to return build info instead of a static version (#42)
    Signed-off-by: everettraven <everettraven@gmail.com>

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@ include .bingo/Variables.mk
 export GIT_VERSION       ?= $(shell git describe --tags --always --dirty)
 export VERSION_PKG       ?= $(shell go list -m)/internal/cli
 export GO_BUILD_ASMFLAGS ?= all=-trimpath=${PWD}
-export GO_BUILD_LDFLAGS  ?= -s -w -X "$(VERSION_PKG).version=$(GIT_VERSION)"
 export GO_BUILD_GCFLAGS  ?= all=-trimpath=${PWD}
 
 export IMAGE_REPO ?= docker.io/bpalmer/buoy
@@ -12,9 +11,8 @@ export IMAGE_TAG  ?= devel
 build:
 	go build \
 	-asmflags '$(GO_BUILD_ASMFLAGS)' \
-	-ldflags '$(GO_BUILD_LDFLAGS)' \
 	-gcflags '$(GO_BUILD_GCFLAGS)' \
-	-o buoy main.go
+	-o buoy .
 
 export ENABLE_RELEASE_PIPELINE ?= false
 export GORELEASER_ARGS         ?= --snapshot --clean
--- internal/cli/version.go
+++ internal/cli/version.go
@@ -2,16 +2,36 @@ package cli
 
 import (
 	"fmt"
+	"runtime/debug"
+	"strings"
 
+	"github.com/charmbracelet/lipgloss"
+	figure "github.com/common-nighthawk/go-figure"
+	"github.com/everettraven/buoy/pkg/charm/styles"
 	"github.com/spf13/cobra"
 )
-
-var version = "unknown"
 
 var versionCommand = &cobra.Command{
 	Use:   "version",
 	Short: "installed version of buoy",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("buoy", version)
+		var out strings.Builder
+		fig := figure.NewFigure("buoy", "rounded", true)
+		header := lipgloss.NewStyle().
+			Border(lipgloss.NormalBorder(), false, false, true).
+			BorderForeground(styles.DefaultColor).
+			Render(fig.String())
+
+		out.WriteString(header + "\n\n")
+		settingNameStyle := lipgloss.NewStyle().Bold(true)
+		if bi, ok := debug.ReadBuildInfo(); ok {
+			for _, setting := range bi.Settings {
+				name := settingNameStyle.Render(setting.Key)
+				out.WriteString(fmt.Sprintf("%s: %s\n", name, setting.Value))
+			}
+		} else {
+			out.WriteString("unable to read build info")
+		}
+		fmt.Print(out.String())
 	},
 }
